1ce6b6b06d0c269d9ae428037585dbb7cf08727a
noakim03 <129218615+noakim03@users.noreply.github.com>
2024-05-14 14:07:55 -0600

    Cambio de componentes y estilo

--- src/components/AgentesIndividual.js
+++ src/components/AgentesIndividual.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import "../images/profile.png";
 
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const AgentesIndividual = ({nombre, apellido}) => {
     const Title = styled.h2`
@@ -11,15 +12,15 @@ const AgentesIndividual = ({nombre, apellido}) => {
 
     `
 
-    return(
+    return (
         <div className='cardW'>
             <div className='estado'></div>
-            <img className='imgPerfil'src={require('../images/profile.png')} alt='Profile'></img>
+            <img className='imgPerfil' src={require('../images/profile.png')} alt='Profile'></img>
             <Title>{nombre} {apellido}</Title>
-            <button className='btnInfo'>Mas</button>
+            <button className='btnInfo'>
+                <Link to="/agente/kpi">Mas</Link>
+            </button>
             <button className='btnInfo'>Comentario</button>
-        
-                    
         </div>
     )
 }
