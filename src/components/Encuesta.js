eae40378f1fefffc7323ee55c3915f58e1b31c77
GRM028 <166761923+GRM028@users.noreply.github.com>
2024-06-03 15:33:42 -0600

    Update Encuesta.js
    Encuesta_final

--- src/components/Encuesta.js
+++ src/components/Encuesta.js
@@ -1,6 +1,6 @@
-import preguntas from './Preguntas';
+import preguntas from './Preguntas'; // obtener el titulo y los estados de las opciones
 import '../styles/encuesta.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Comentario from './Comentario';
 
 function Encuesta() {
@@ -10,35 +10,34 @@ function Encuesta() {
   const [puntajesYcomentarios, setPuntajesYcomentarios] = useState([]);
   const [mostrarComentario, setMostrarComentario] = useState(false);
   const [mostrarPregunta, setMostrarPregunta] = useState(true);
+  const [puntajesAcumulados, setPuntajesAcumulados] = useState([]);
+  const lugar = "localhost";
+  const puerto = "8085";
 
-
-  function handlePuntuacion(isVerdad, isPart, isFalso){
-    if (isVerdad){
+  function handlePuntuacion(isVerdad, isPart, isFalso) {
+    if (isVerdad) {
       setPuntuacion(puntuacion + 1);
       setMostrarPregunta(false);
       setMostrarComentario(true);
-    }else if (isPart){
+    } else if (isPart) {
       setPuntuacion(puntuacion + 0.5);
       setMostrarPregunta(false);
       setMostrarComentario(true);
-    }else if (isFalso){
+    } else if (isFalso) {
       setPuntuacion(puntuacion + 0);
       setMostrarPregunta(false);
       setMostrarComentario(true);
+    }
   }
- }
 
   function handleComentarioSubmit(comentario) {
-    if (comentario.trim() === ''){
-      alert("El comentario no puede estar vacío.");
-      return;
-    }
-    
     const nuevaEntrada = {
+      username: localStorage.getItem('Username'),
       puntaje: puntuacion,
       comentario: comentario,
     };
     setPuntajesYcomentarios([...puntajesYcomentarios, nuevaEntrada]);
+    setPuntajesAcumulados([...puntajesAcumulados, puntuacion]);
     setMostrarPregunta(true);
     setMostrarComentario(false);
     setPuntuacion(0);
@@ -50,22 +49,50 @@ function Encuesta() {
     }
   }
 
-  if (fin) {
-    console.log("Puntajes y comentarios:");
-    puntajesYcomentarios.forEach((entrada, index) => {
-      console.log(`Pregunta ${index + 1}: Puntaje ${entrada.puntaje}`);
-      if (entrada.comentario) {
-        console.log(`Comentario: ${entrada.comentario}`);
-      }
-    });
+  useEffect(() => {
+    if (fin) {
+      const puntajeTotalAcumulado = puntajesAcumulados.reduce((total, puntaje) => total + puntaje, 0);
+      const username = localStorage.getItem('Username');
+      const comentarios = puntajesYcomentarios.map(entrada => entrada.comentario);
 
+      const resultadosEncuestaJSON = JSON.stringify({
+        username: username,
+        score: puntajeTotalAcumulado,
+        comment: comentarios
+      }, null, 2);
+
+      const enviardatos = async (resultadosEncuestaJSON) => {
+        const enviar = {
+          method: 'POST',
+          body: resultadosEncuestaJSON,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        };
+
+        try {
+          const enviarC = await fetch("http://" + lugar + ":" + puerto + "/EncuestaModel/postEncuestaPipeline", enviar);
+          if (!enviarC.ok) {
+            const text = await enviarC.text();
+            console.log("Error en la respuesta del servidor", enviarC.status, enviarC.statusText, text);
+            return;
+          }
+        } catch (error) {
+          console.log('error');
+        }
+      };
+      enviardatos(resultadosEncuestaJSON);
+    }
+  }, [fin]);
+
+  if (fin) {
     return (
       <div className='Encuesta'>
         <div className='finEncuesta'>
           <span className='letrero'>
-            ENCUESTA FINALIZADA
+            SURVEY COMPLETED
           </span>
-          <button className="env" onClick={() => (window.location.href="/agentes")}>Enviar</button>
+          <button className="env" onClick={() => (window.location.href = "/agentes")}>Submit</button>
         </div>
       </div>
     );
@@ -74,23 +101,24 @@ function Encuesta() {
   return (
     <div className="Encuesta">
       {mostrarPregunta && (
-        <div className="lado-izquierdo">
+        <div className="arriba">
           <div className="numero-pregunta">
-            <span>Pregunta {preguntaActual + 1} de {preguntas.length} </span>
+            <span>Question {preguntaActual + 1} of {preguntas.length} </span>
           </div>
           <div className="titulo-pregunta">{preguntas[preguntaActual].titulo}</div>
         </div>
       )}
 
-      <div className="lado-derecho">
+      <div className="abajo">
         {preguntas[preguntaActual].opciones.map((respuesta) => (
-          <button className='env' key={respuesta.textoRespuesta} onClick={() => handlePuntuacion(respuesta.isVerdad, respuesta.isPart, respuesta.isFalso)} style={{ display: mostrarPregunta ? 'block' : 'none' }}>
-            {respuesta.textoRespuesta}
+          <button className="btn" key={respuesta.textoRespuesta}
+            onClick={() => handlePuntuacion(respuesta.isVerdad, respuesta.isPart, respuesta.isFalso)}
+            style={{ display: mostrarPregunta ? 'block' : 'none' }}> {respuesta.textoRespuesta}
           </button>
         ))}
       </div>
 
-      {mostrarComentario && <Comentario onSubmit={handleComentarioSubmit}/>}
+      {mostrarComentario && <Comentario onSubmit={handleComentarioSubmit} />}
     </div>
   );
 }
