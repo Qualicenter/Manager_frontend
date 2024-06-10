4e48e032a16b2e90112ac5e09cd251ff2b57820e
Chabelo <A01753830@tec.mx>
2024-06-09 18:35:18 -0600

    Documentados

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -4,15 +4,17 @@
 import React, { useState } from 'react';
 
 function Comentario({ onSubmit }) {
-  const [comentario, setComentario] = useState('');
-  const [comentarioValido, setComentarioValido] = useState(false);
+  const [comentario, setComentario] = useState('');//Se almacena el texto del comentario
+  const [comentarioValido, setComentarioValido] = useState(false); // indica si el comentario es válido
 
+  //función que actualiza el estado de comentario e indica si el comentario no esta vacío
   const handleChangeComentario = (event) => {
     const comentarioText = event.target.value;
     setComentario(comentarioText);
     setComentarioValido(comentarioText.trim() !== ''); 
   };
 
+  //función que verifica si el comentario es valído y reinicia el estado del comentario y su validación
   const handleSubmit = () => {
     if (!comentarioValido) {
       return 
