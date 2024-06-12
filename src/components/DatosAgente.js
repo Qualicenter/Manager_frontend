4ae1f8e4123644e02b3b25ddd5ebd49e21481daf
noakim03 <129218615+noakim03@users.noreply.github.com>
2024-06-12 17:36:54 -0600

    Modificación de comentario del tiempo de descarga

--- src/components/DatosAgente.js
+++ src/components/DatosAgente.js
@@ -39,7 +39,7 @@ const Datos = ({ info }) => {
     }
   }, [nombreAgente, setAgente]);
 
-  /* Download the agent's information every 10 seconds */
+  /* Download the agent's information every 15 seconds */
   useEffect(() => {
     descargarInfoAgente();
     const intervalo = setInterval(descargarInfoAgente, 15000);
